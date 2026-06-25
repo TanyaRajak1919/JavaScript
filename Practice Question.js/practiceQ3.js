80947e6c815190e18097bf082b280f88f37c3a01
TanyaRajak1919 <tanyarajak4646@gmail.com>
2026-06-26 04:02:15 +0530

    loops Q

---
+++ Practice Question.js/practiceQ3.js
@@ -0,0 +1,11 @@
+
+// for(let count=1; count <=10; count++){
+//  console.log("tanyaa");
+
+// }
+let sum = 0;
+for(let i =1; i<=5;i++){
+    sum=sum+1;
+}
+console.log("sum = ",sum);
+console.log("loop has ended");
