1f36d4a2101c42fff96012e7f844edd490d05d64
TanyaRajak1919 <tanyarajak4646@gmail.com>
2026-07-09 18:08:57 +0530

    foreachloop

---
+++ Arrowfun.js
@@ -0,0 +1,8 @@
+const arrowsum = (a,b) =>{
+    return a+b;
+
+}
+const arrorwSum = (a,b) =>{
+    console.log(a+b);
+};
+
---
+++ foreachloop.js
@@ -0,0 +1,12 @@
+
+// function myFunc(abc){
+//     return abc;
+// unction abc(){
+//     console.log("hello");
+// }
+//}
+//example 
+let arr =[1, 2, 3, 4, 5];
+arr.forEach(function printVal(val){
+    console.log(val);
+});
