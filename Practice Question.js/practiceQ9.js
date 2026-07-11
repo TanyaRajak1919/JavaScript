b24c260d97eda71a83e02b9d964241df52232fb0
TanyaRajak1919 <tanyarajak4646@gmail.com>
2026-07-11 15:40:26 +0530

    ptQ9

---
+++ Practice Question.js/practiceQ9.js
@@ -0,0 +1,7 @@
+let num1 = 8;
+let num2 = 2;
+
+console.log("sum", num1 +num2);
+console.log("subtract", num1 - num2);
+console.log("multiplication", num1 * num2);
+console.log ("division", num1 /num2);
